30e14f5fadc8ea63db014be8f3c54af9c977397d
Hanna <marmalademarmoset@gmail.com>
2023-05-17 12:25:30 -0700

    Adding RESTful routes for crafts

--- app/controllers/crafts_controller.rb
+++ app/controllers/crafts_controller.rb
@@ -1,6 +1,32 @@
 class CraftsController < ApplicationController
 
-    
+    get '/crafts' do
+        crafts = Craft.all
+        crafts.to_json
+    end
+
+    get '/crafts/:id' do
+        craft = Craft.find(params[:id])
+        craft.to_json
+    end
+
+    patch '/crafts/:id' do
+        craft = Craft.find(params[:id])
+        craft.update(
+            name: params[:name],
+            image: params[:image],
+            difficulty: params[:difficulty],
+            description: params[:description],
+            notes: params[:notes],
+            category_id: params[:category_id],
+            completed: params[:completed]
+        )
+    end
+
+    delete '/crafts/:id' do
+        craft = Craft.find(params[:id])
+        craft.destroy
+    end
 
 
 
